40e0f8da4e1e8cef3f29332dd07586ccfd7f012e
Jakub Kuc <jakub.kuc.11@gmail.com>
2026-01-04 18:48:40 +0100

    fix

--- KorID.Data/Migrations/20260104173533_AddUserAuth.Designer.cs
+++ KorID.Data/Migrations/20260104173533_AddUserAuth.Designer.cs
@@ -2,6 +2,7 @@
 using KorID.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace KorID.Data.Migrations
 {
     [DbContext(typeof(KorIdDbContext))]
-    partial class KorIdDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260104173533_AddUserAuth")]
+    partial class AddUserAuth
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
